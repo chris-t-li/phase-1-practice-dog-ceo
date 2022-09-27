af1a91f9f1cd71cfd0bada98d38b733823ec0921
Chris Li <chris.lty07@gmail.com>
2022-09-26 22:36:06 -0400

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -1,57 +1,70 @@
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+const breedUrl = 'https://dog.ceo/api/breeds/list/all';
+const dogContainer = document.getElementById('dog-image-container');
+const dogBreeds = document.getElementById('dog-breeds');
 
-const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
-const breedUrl = "https://dog.ceo/api/breeds/list/all"
-
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
     // Challenge 1
     fetch(imgUrl)
-    .then(res => res.json())
-    .then(data => {
-        for(const image in data.message) {
-            const img = document.createElement('img');
-            img.src = data.message[image];
-            document.getElementById('dog-image-container').append(img);
-        }
-    })
-
-    // Challenge 2
-        fetch(breedUrl)
         .then(res => res.json())
         .then(data => {
-            for(const breed in data.message) {
-                if(data.message[breed].length === 0) {
-                    const li = document.createElement('li');
+            data.message.forEach(image => {
+                const dogImage = document.createElement('img');
+                dogImage.src = image;
+                dogContainer.append(dogImage);
+            })
+        })
+
+    // Challenge 2
+    // fetch(breedUrl)
+    // .then(res => res.json())
+    // .then(data => {
+    //     for(const breed in data.message) {
+    //         const li = document.createElement('li');
+    //         li.textContent = breed;
+    //         dogBreeds.append(li);
+    //     }    
+    // })
+
+    // Challenge 2 - Elaborate
+    fetch(breedUrl)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data.message);
+            const breedList = data.message;
+
+            for (const breed in breedList) {
+                if (breedList[breed].length === 0) {
+                    const li = document.createElement('li')
                     li.textContent = breed;
-                    document.getElementById('dog-breeds').append(li);
+                    dogBreeds.append(li);
                 } else {
-                    for(let i=0; i< data.message[breed].length; i++) {
-                        const li = document.createElement('li');
-                        li.textContent = `${data.message[breed][i]} ${breed}`;
-                        document.getElementById('dog-breeds').append(li);
+                    for (let i = 0; i < breedList[breed].length; i++) {
+                        const li = document.createElement('li')
+                        li.textContent = `${breedList[breed][i]} ${breed}`;
+                        dogBreeds.append(li);
                     }
-                }   
+                }
             }
-        })
 
-    // Challenge 3
-    document.getElementById('dog-breeds').addEventListener('click', e => {
-        e.target.style.color = 'red';
-    })
+            // Challenge 3
+            dogBreeds.addEventListener('click', e => {
+                e.target.style.color = "red";
+            })
 
-    // Challenge 4
-    document.getElementById('breed-dropdown').addEventListener('change', e => {
-        const list = document.querySelectorAll('li');
-        list.forEach(element => element.style.display = 'list-item')
-        list.forEach(element => {
-            if(element.textContent[0] !== e.target.value) {
-                element.style.display = 'none';
-            }
+            // Challenge 4
+            document.getElementById('breed-dropdown').addEventListener('change', e => {
+                const list = document.querySelectorAll('li');
+                list.forEach(element => element.style.display = 'list-item')
+                list.forEach(element => {
+                    if (element.textContent[0] !== e.target.value) {
+                        element.style.display = 'none'
+                    }
+                })
+
+                if (document.getElementById('breed-dropdown').value === 'all') {
+                    list.forEach(element => element.style.display = 'list-item');
+                }
+            })
         })
-        if(document.getElementById('breed-dropdown').value === 'all') {
-            list.forEach(element => element.style.display = 'list-item')
-        }
-    })
 })
-
-
-
